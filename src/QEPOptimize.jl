e6e70ac5d849ebaa37afdf2700a95eebac33a670
xoth42 <xoth42@protonmail.com>
2026-01-30 23:22:34 -0500

    Updated pluto to use relaxation noise

--- src/QEPOptimize.jl
+++ src/QEPOptimize.jl
@@ -24,7 +24,7 @@ using ProgressLogging: @progress
 
 using QuantumClifford: SparseGate,Tableau,sCNOT,BellMeasurement,Reset,sMX,sMZ,sMY,Stabilizer,apply!,MixedDestabilizer,AbstractCliffordOperator, AbstractOperation
 
-export Individual, calculate_performance!, f_in_to_pauli, NetworkFidelity, NetworkPauliNoise, MeasurementError, T1T2Noise, Population, # TODO order these neatly
+export Individual,initialize_pop!, calculate_performance!, f_in_to_pauli, NetworkFidelity, NetworkPauliNoise, MeasurementError, T1T2Noise, Population, # TODO order these neatly
     multiple_steps_with_history!,
     analyze_f_out_vs_f_in, plot_circuit_analysis, plot_fitness_history
 
